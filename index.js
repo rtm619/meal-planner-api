9102dcc402836d4a46ee68a3c802309cd8b78519
Reetam Chatterjee <rtm619@gmail.com>
2020-07-04 10:21:57 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+
+router.post('/sign-in', (req, res) => {
+
+})
+
+router.post('/sign-up', (req, res) => {
+  
+})
+
+router.get('/sign-out', (req, res) => {
+
+})
+
+module.exports = router
